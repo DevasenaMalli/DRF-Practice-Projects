06eb2d5f108ee808d892f6f96608f406a94f7a96
Devasena Malli <devasena.malli@gmail.com>
2024-07-04 20:15:41 -0400

    cbvassignment

--- CBVAssingment/cbvApp/urls.py
+++ CBVAssingment/cbvApp/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-urlpatterns = [
+# urlpatterns = [
  
-    path('courses/',views. CourseList.as_view()),
-    path('courses/<int:pk>',views.CourseDetails.as_view()),
+#     path('courses/',views. CourseList.as_view()),
+#     path('courses/<int:pk>',views.CourseDetails.as_view()),
     
-]
+# ]
